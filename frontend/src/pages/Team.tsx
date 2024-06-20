95d960ba7f082fc554568380685e79768b1020fa
Egecan Akıncıoğlu <egecanakincioglu@icloud.com>
2024-06-20 14:53:59 +0300

    Team Updated

--- frontend/src/pages/Team.tsx
+++ frontend/src/pages/Team.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Person from '../components/Person';
+import HeaderElement from '../components/HeaderElement';
 
 const Team: React.FC = () => {
   return (
-    <div className="container mx-auto p-4">
+    <HeaderElement>
       <h1 className="text-4xl font-bold mb-4">Our Team</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
         {/* Teknik kadronun üyeleri */}
@@ -12,7 +13,7 @@ const Team: React.FC = () => {
         </div>
         {/* Diğer ekip üyeleri */}
       </div>
-    </div>
+    </HeaderElement>
   );
 }
 
