292678a521212b5dc8254123008e2b42b920f2d1
Egecan Akıncıoğlu <egecanakincioglu@icloud.com>
2024-06-21 01:46:35 +0300

    API Files updated

--- backend/src/main/java/com/ordem/api/admin/Requests.java
+++ backend/src/main/java/com/ordem/api/admin/Requests.java
@@ -1,12 +1,12 @@
-package com.ordem.api;
+package com.ordem.api.admin;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/projects")
-public class Projects {
+@RequestMapping("/api/requests")
+public class Requests {
     @GetMapping
     public String getAllUsers() {
         return "Hello, World!";
--- backend/src/main/java/com/ordem/api/general/Projects.java
+++ backend/src/main/java/com/ordem/api/general/Projects.java
@@ -1,12 +1,12 @@
-package com.ordem.api;
+package com.ordem.api.general;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/requests")
-public class Requests {
+@RequestMapping("/api/projects")
+public class Projects {
     @GetMapping
     public String getAllUsers() {
         return "Hello, World!";
