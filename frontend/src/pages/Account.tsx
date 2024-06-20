ee62d193791366d8974465c473ccfdbd3470c591
Egecan Akıncıoğlu <egecanakincioglu@icloud.com>
2024-06-20 14:54:56 +0300

    Account menu Added

--- frontend/src/pages/Account.tsx
+++ frontend/src/pages/Account.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import HeaderElement from '../components/HeaderElement';
 
 const Account: React.FC = () => {
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold">Your Account</h1>
-      {/* Hesap detayları ve yönetimi buraya gelecek */}
-    </div>
+    <HeaderElement> 
+      <h1 className="text-2xl font-bold">Your Account</h1> 
+    </HeaderElement>
   );
 }
 
