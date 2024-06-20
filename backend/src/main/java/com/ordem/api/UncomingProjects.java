6451d1ea1ebd6c67a6345304f00229f26da56fcf
Egecan Akıncıoğlu <egecanakincioglu@icloud.com>
2024-06-20 15:45:32 +0300

    UncomingProjects added to API

---
+++ backend/src/main/java/com/ordem/api/UncomingProjects.java
@@ -0,0 +1,14 @@
+package com.ordem.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/uncoming-projects")
+public class UncomingProjects {
+    @GetMapping
+    public String getAllUsers() {
+        return "Hello, World!";
+    }
+}
