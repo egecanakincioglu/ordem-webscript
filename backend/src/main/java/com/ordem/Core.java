49aaa6124648e9aea189b82f0da1bd0ef5141b46
Egecan Akıncıoğlu <egecanakincioglu@icloud.com>
2024-06-21 23:21:58 +0300

    Test Method added

--- backend/src/main/java/com/ordem/Core.java
+++ backend/src/main/java/com/ordem/Core.java
@@ -1,12 +1,18 @@
 package com.ordem;
 
+import com.ordem.services.JsonService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Core {
 
     public static void main(String[] args) {
-        SpringApplication.run(Core.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(Core.class, args);
+        JsonService jsonService = context.getBean(JsonService.class);
+
+        jsonService.saveJsonData("test", 1);
+        System.out.println("Başarılı");
     }
 }
