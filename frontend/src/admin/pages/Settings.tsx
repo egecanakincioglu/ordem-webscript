65fb9207e77b3744ec2477b0908bdc62cb76ee8a
Egecan Akıncıoğlu <egecanakincioglu@icloud.com>
2024-06-21 23:23:30 +0300

    Dashboard settings category added

---
+++ frontend/src/admin/pages/Settings.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Settings: React.FC = () => {
+  return (
+    <div>
+      <h1>Ayarlar</h1>
+      <p>Yönetim paneli ayarları burada olacak.</p>
+    </div>
+  );
+}
+
+export default Settings;
