5711740e999516d69b27fa91548b5856192d6ed7
Egecan Akıncıoğlu <egecanakincioglu@icloud.com>
2024-06-21 23:23:17 +0300

    dashboard Main page added

---
+++ frontend/src/admin/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Dashboard Ana Sayfa</h1>
+      <p>Genel bakış ve istatistikler burada olacak.</p>
+    </div>
+  );
+}
+
+export default Home;
